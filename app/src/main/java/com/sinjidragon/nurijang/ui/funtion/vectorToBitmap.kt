40a86532d4ac19663446da8e60b659a922733bc6
iszero12 <binj1246@gmail.com>
2024-11-07 22:04:45 +0900

    fix :: add shadow from vectorToBitmap

--- app/src/main/java/com/sinjidragon/nurijang/ui/funtion/vectorToBitmap.kt
+++ app/src/main/java/com/sinjidragon/nurijang/ui/funtion/vectorToBitmap.kt
@@ -3,22 +3,28 @@ package com.sinjidragon.nurijang.ui.funtion
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.drawable.VectorDrawable
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 
 
-fun vectorToBitmap(context: Context, vectorDrawableId: Int): Bitmap {
-    // 벡터 드로어블 로드
+fun vectorToBitmap(context: Context, vectorDrawableId: Int, isShadow: Boolean = false): Bitmap {
     val vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableId) as VectorDrawable
-
-    // 비트맵 생성
-    val bitmap = Bitmap.createBitmap(vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val bitmap = Bitmap.createBitmap(
+        vectorDrawable.intrinsicWidth,
+        vectorDrawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
     val canvas = Canvas(bitmap)
-
-    // 벡터 이미지를 캔버스에 그리기
-    vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+    if (isShadow) {
+        val paint = Paint().apply {
+            color = Color.BLACK
+            setShadowLayer(20f, 0f, 0f, Color.argb(64, 0, 0, 0)) // 블러 반경 8, 검정색, 투명도 25% (ARGB 64)
+        }
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        canvas.drawBitmap(bitmap, 0f, 0f, paint)
+    }
     vectorDrawable.draw(canvas)
-
     return bitmap
 }
