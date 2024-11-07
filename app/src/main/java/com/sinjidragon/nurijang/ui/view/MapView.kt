7109409568364d797802ee9083d912149273c299
iszero12 <binj1246@gmail.com>
2024-11-07 20:13:55 +0900

    feat :: add GoogleMap

--- app/src/main/java/com/sinjidragon/nurijang/ui/view/MapView.kt
+++ app/src/main/java/com/sinjidragon/nurijang/ui/view/MapView.kt
@@ -3,15 +3,20 @@ package com.sinjidragon.nurijang.ui.view
 import android.content.pm.PackageManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
+import com.google.maps.android.compose.GoogleMap
 import com.sinjidragon.nurijang.ui.theme.NurijangTheme
 
 @Composable
@@ -34,6 +39,15 @@ fun MapView() {
     LaunchedEffect(Unit) {
         launcher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
     }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize()
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+    )
 }
 @Preview(showBackground = true)
 @Composable
