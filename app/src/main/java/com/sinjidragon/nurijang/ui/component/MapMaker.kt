bf511fe7ba5238611507659fa4b792eec5d00582
iszero12 <binj1246@gmail.com>
2024-11-08 08:35:54 +0900

    feat :: create custom marker

---
+++ app/src/main/java/com/sinjidragon/nurijang/ui/component/MapMaker.kt
@@ -0,0 +1,26 @@
+package com.sinjidragon.nurijang.ui.component
+
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.sinjidragon.nurijang.ui.funtion.vectorToBitmap
+
+@Composable
+fun MapMarker(
+    context: Context,
+    position: LatLng,
+    title: String,
+    @DrawableRes iconResourceId: Int
+) {
+    val icon = vectorToBitmap(
+        context, iconResourceId,130,130
+    )
+    Marker(
+        state = MarkerState(position = position),
+        title = title,
+        icon = icon,
+    )
+}
