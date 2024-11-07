a3ca588f6380632624d18cbb735a13dd61cf2d05
iszero12 <binj1246@gmail.com>
2024-11-07 21:34:30 +0900

    fix :: add now_location marker

--- app/src/main/java/com/sinjidragon/nurijang/ui/view/MapView.kt
+++ app/src/main/java/com/sinjidragon/nurijang/ui/view/MapView.kt
@@ -1,6 +1,11 @@
 package com.sinjidragon.nurijang.ui.view
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.VectorDrawable
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
@@ -15,8 +20,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.sinjidragon.nurijang.R
+import com.sinjidragon.nurijang.ui.funtion.vectorToBitmap
 import com.sinjidragon.nurijang.ui.theme.NurijangTheme
 
 @Composable
@@ -36,13 +54,34 @@ fun MapView() {
             hasPermission = isGranted
         }
     )
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+    val cameraPositionState = rememberCameraPositionState()
+    val nowLocation = remember { mutableStateOf<LatLng?>(null) }
     LaunchedEffect(Unit) {
         launcher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val latLng = LatLng(it.latitude, it.longitude)
+                nowLocation.value = latLng
+            }
+        }
     }
 
     GoogleMap(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
     )
+    {
+        nowLocation.value?.let { location ->
+            Marker(
+                state = MarkerState(location),
+                title = "현재 위치",
+                snippet = "이곳이 현재 위치입니다.",
+                icon = BitmapDescriptorFactory.fromBitmap(vectorToBitmap(context,R.drawable.now_location_icon))
+            )
+        }
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
