5f5025548c315c3bdbcf9f33b661fc9a1324f23c
iszero12 <binj1246@gmail.com>
2024-11-08 10:48:43 +0900

    feat :: add text from vectorToBitmap

--- app/src/main/java/com/sinjidragon/nurijang/ui/funtion/vectorToBitmap.kt
+++ app/src/main/java/com/sinjidragon/nurijang/ui/funtion/vectorToBitmap.kt
@@ -2,20 +2,56 @@ package com.sinjidragon.nurijang.ui.funtion
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.util.TypedValue
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import android.graphics.Color as AndroidColor
 
 fun vectorToBitmap(
     context: Context,
     vectorResId: Int,
     width: Int,
-    height: Int
+    height: Int,
+    text: String? = null,
+    isText: Boolean = true
 ): BitmapDescriptor? {
     val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
     drawable.setBounds(0, 0, width, height)
-    val bm = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val paint = Paint().apply {
+        color = AndroidColor.parseColor("#11235A")
+        textSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP, 17f, context.resources.displayMetrics
+        )
+        typeface = Typeface.create("pretendard", Typeface.NORMAL)
+        textAlign = Paint.Align.CENTER
+    }
+    val textHeight = paint.fontMetrics.descent - paint.fontMetrics.ascent
+    val textWidth = if (text != null) paint.measureText(text) else 0f
+    val totalWidth = maxOf(width, textWidth.toInt() + 20)
+    val totalHeight = if (text != null && isText) {
+        height + textHeight.toInt() + 20
+    } else {
+        height
+    }
+    val bm = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888)
     val canvas = android.graphics.Canvas(bm)
+    val iconXPos = (totalWidth - width) / 2
+    val iconYPos = (totalHeight - height - textHeight.toInt()) / 2
+    drawable.setBounds(iconXPos, iconYPos, iconXPos + width, iconYPos + height)
     drawable.draw(canvas)
+    if (text != null && isText) {
+        val xPos = (canvas.width / 2).toFloat()
+        val yPos = iconYPos + height + textHeight / 2
+        canvas.drawText(text, xPos, yPos, paint)
+    }
     return BitmapDescriptorFactory.fromBitmap(bm)
 }
+
+
+
+
+
+
