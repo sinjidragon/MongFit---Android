4bbfd0bf5e2f304158eee4fb298546e96f1935d9
iszero12 <binj1246@gmail.com>
2024-11-10 16:01:35 +0900

    fix :: Fix issue with odd size

--- app/src/main/java/com/sinjidragon/nurijang/ui/component/CurrentLocationMaker.kt
+++ app/src/main/java/com/sinjidragon/nurijang/ui/component/CurrentLocationMaker.kt
@@ -2,6 +2,7 @@ package com.sinjidragon.nurijang.ui.component
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.util.TypedValue
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
 import androidx.core.content.ContextCompat
@@ -11,14 +12,24 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 
-fun currentVectorToBitmap(context: Context, vectorResId: Int, width: Int, height: Int): BitmapDescriptor? {
+fun currentVectorToBitmap(context: Context, vectorResId: Int, widthDp: Float, heightDp: Float): BitmapDescriptor? {
+    val widthPx = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, widthDp, context.resources.displayMetrics
+    ).toInt()
+    val heightPx = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, heightDp, context.resources.displayMetrics
+    ).toInt()
+
     val drawable = ContextCompat.getDrawable(context, vectorResId) ?: return null
-    drawable.setBounds(0, 0, width, height) // 벡터 이미지의 크기를 적절히 설정
-    val bm = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    drawable.setBounds(0, 0, widthPx, heightPx)
+
+    val bm = Bitmap.createBitmap(widthPx, heightPx, Bitmap.Config.ARGB_8888)
     val canvas = android.graphics.Canvas(bm)
     drawable.draw(canvas)
+
     return BitmapDescriptorFactory.fromBitmap(bm)
 }
+
 @Composable
 fun CurrentLocationMarker(
     context: Context,
@@ -26,7 +37,7 @@ fun CurrentLocationMarker(
     @DrawableRes iconResourceId: Int
 ) {
     val icon = currentVectorToBitmap(
-        context, iconResourceId,160,160
+        context, iconResourceId,33f,33f
     )
     Marker(
         state = MarkerState(position = position),
