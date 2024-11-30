90ef0e72bbcb2cd6417ecfdbbb1cb6c099142846
iszero12 <binj1246@gmail.com>
2024-11-30 16:35:36 +0900

    fix :: Adjust timeout time

--- app/src/main/java/com/sinjidragon/nurijang/remote/Client.kt
+++ app/src/main/java/com/sinjidragon/nurijang/remote/Client.kt
@@ -4,9 +4,11 @@ import com.sinjidragon.nurijang.BuildConfig
 import com.sinjidragon.nurijang.remote.service.ChatBotService
 import com.sinjidragon.nurijang.remote.service.FacilityService
 import com.sinjidragon.nurijang.remote.service.SearchService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Client {
     private var retrofit: Retrofit? = null
@@ -14,8 +16,17 @@ object Client {
     private fun getClient(): Retrofit {
         if (retrofit == null) {
             val url = BuildConfig.BASE_URL
+
+
+            val okHttpClient = OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .build()
+
             retrofit = Retrofit.Builder()
                 .baseUrl(url)
+                .client(okHttpClient)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
