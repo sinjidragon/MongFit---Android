e2e666a100dec4415b06a640afa7efc6ce7c91bd
iszero12 <binj1246@gmail.com>
2024-12-02 19:43:44 +0900

    fix :: Convert to facility object equal

--- app/src/main/java/com/sinjidragon/nurijang/remote/data/Facility.kt
+++ app/src/main/java/com/sinjidragon/nurijang/remote/data/Facility.kt
@@ -33,4 +33,23 @@ data class Facility(
     override fun hashCode(): Int {
         return Objects.hashCode(1)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Facility
+
+        if (id != other.id) return false
+        if (distance != other.distance) return false
+        if (fcltyNm != other.fcltyNm) return false
+        if (fcltyAddr != other.fcltyAddr) return false
+        if (fcltyDetailAddr != other.fcltyDetailAddr) return false
+        if (rprsntvTelNo != other.rprsntvTelNo) return false
+        if (mainItemNm != other.mainItemNm) return false
+        if (fcltyCrdntLo != other.fcltyCrdntLo) return false
+        if (fcltyCrdntLa != other.fcltyCrdntLa) return false
+
+        return true
+    }
 }
